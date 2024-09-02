c77b1d6c184a691c2f2b5d4cda8116d46625360e
Frans David Aguirre Rueda <dark.tempus@gmail.com>
2024-09-02 16:46:20 -0500

    uso de params httpp

--- src/app/services/gifs.service.ts
+++ src/app/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -10,8 +10,10 @@ export class GifsService {
     private http: HttpClient
   ) { }
   private _tagsHistory: string[] = [];
-
   private apiKey: string = 'fxpQClUSxdDsbEJGmBB7RgXLWyAv6ouP';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs/'
+
+  // limit: number = 10;
   private organizeHistory(tag: string) {
     tag = tag.toLowerCase();
     if (this._tagsHistory.includes(tag)) {
@@ -28,12 +30,19 @@ export class GifsService {
   public searchTag(tag: string): void {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${tag}&limit=10`).
-    subscribe(Response => {
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', 'tag')
+
+    // this.http.get(`${this.serviceUrl}search?api_key=${this.apiKey}&q=${tag}&limit=${this.limit}`).
+    this.http.get(`${this.serviceUrl}search?`, { params }).
+      subscribe(Response => {
         console.log(Response);
       }
 
-    )
+      )
 
 
     // this._tagsHistory.unshift(tag);
